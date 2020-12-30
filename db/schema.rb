160cac48f0ac289602f5377245a92e6e5850fe7f
kolan4ick <Kolan4ick@gmail.com>
2020-12-30 22:53:51 +0200

    Lesson 5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_115144) do
+ActiveRecord::Schema.define(version: 2020_12_30_192742) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_12_28_115144) do
     t.integer "user_id"
     t.integer "views"
     t.integer "category_id"
+    t.integer "likes_count"
   end
 
   create_table "users", force: :cascade do |t|
